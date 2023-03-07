fc76075085ad691d6cb8527655b6a28af3a62578
Guilherme Samuel <guilhermesnd14@gmail.com>
2023-03-07 17:57:09 -0300

    fix: remove unnecessary code

--- app.py
+++ app.py
@@ -2,15 +2,6 @@ from flask import Flask, render_template, request
 from fairseq.checkpoint_utils import load_model_ensemble_and_task_from_hf_hub
 from fairseq.models.text_to_speech.hub_interface import TTSHubInterface
 import IPython.display as ipd
-import re
-
-# Define a regular expression to match sentence-ending punctuation
-SENTENCE_END_RE = re.compile(r'[.?!]\s+')
-
-def add_sentence_end(text):
-    # Add a period after each sentence-ending punctuation mark
-    return SENTENCE_END_RE.sub('. ', text.strip()) + '.'
-
 
 models, cfg, task = load_model_ensemble_and_task_from_hf_hub(
     "facebook/fastspeech2-en-ljspeech",
@@ -24,7 +15,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    text = add_sentence_end(request.args.get('text', ''))
+    text = request.args.get('text', '')
     audio_html = ''
     if text:
         sample = TTSHubInterface.get_model_input(task, text)
@@ -33,7 +24,5 @@ def index():
         audio = ipd.Audio(wav, rate=rate)
 
         audio_html = audio._repr_html_()
-
-        print(audio_html, ' <audio')
     
     return render_template('index.html', audio_html=audio_html, text=text)
