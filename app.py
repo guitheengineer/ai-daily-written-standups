33cf1e66164c40d89d8bbbf36cade3983693b9df
guilhermefront <guilhermesnd14@gmail.com>
2023-03-18 12:28:07 -0300

    wip: format items

--- app.py
+++ app.py
@@ -1,26 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    # person_profile = {
-    #     name: '',
-    #     about: '',
-    #     work_experience: [{
-    #         year: '',
-    #         title: '',
-    #         company: '',
-    #         description: ''
-    #     }],
-    #        education: [{
-    #         year: '',
-    #         course: '',
-    #         level: '',
-    #         company: '',
-    #         description: ''
-    #     }], softSkills: [''],
-    #     hardSkills: ['']
-    # }
+    body = request.args.to_dict()
+    person_profile = dict()
+    person_profile["work_experience"] = []
+
+    for key in body:
+        if "job" in key:
+            jobIndex = int(key.split("-")[0].replace("job", ""))
+            jobKey = key.split("-")[1]
+            person_profile["work_experience"].insert(jobIndex, {jobKey: body[key]})
+        else:
+            person_profile[key] = body[key]
+
+    print(person_profile)
+
+    # about: '',
+    # work_experience: [{
+    #     year: '',
+    #     title: '',
+    #     company: '',
+    #     description: ''
+    # }],
+    #    education: [{
+    #     year: '',
+    #     course: '',
+    #     level: '',
+    #     company: '',
+    #     description: ''
+    # }], softSkills: [''],
+    # hardSkills: ['']
+
     return render_template("index.html")
