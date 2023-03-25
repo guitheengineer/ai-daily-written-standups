a9da2724ad73ea31ae4cb57dad28937ef30e9a69
guilhermefront <guilhermesnd14@gmail.com>
2023-03-25 17:57:01 -0300

    feat: add missing features for app

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from flask import Flask, render_template, request
+import os
+import openai
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 app = Flask(__name__)
 
@@ -7,17 +11,19 @@ app = Flask(__name__)
 def index():
     body = request.args.to_dict()
     person_profile = dict()
-    person_profile["work_experience"] = []
+    person_profile["workExperience"] = []
     person_profile["education"] = []
+
+    result = ""
 
     for key in body:
         if "job" in key:
             jobIndex = int(key.split("-")[0].replace("job", ""))
             jobKey = key.split("-")[1]
             try:
-                person_profile["work_experience"][jobIndex - 1][jobKey] = body[key]
+                person_profile["workExperience"][jobIndex - 1][jobKey] = body[key]
             except:
-                person_profile["work_experience"].insert(jobIndex, {jobKey: body[key]})
+                person_profile["workExperience"].insert(jobIndex, {jobKey: body[key]})
 
         elif "education" in key:
             jobIndex = int(key.split("-")[0].replace("education", ""))
@@ -30,20 +36,13 @@ def index():
         else:
             person_profile[key] = request.args.getlist(key)
 
-    # about: '',
-    # work_experience: [{
-    #     year: '',
-    #     title: '',
-    #     company: '',
-    #     description: ''
-    # }],
-    #    education: [{
-    #     year: '',
-    #     course: '',
-    #     level: '',
-    #     company: '',
-    #     description: ''
-    # }], softSkills: [''],
-    # hardSkills: ['']
+    if len(body.values()) > 0:
+        response = openai.Completion.create(
+            model="text-curie-001",
+            prompt=f"Generate a cover letter for me in first person. Here is some profile information about myself and the company as an object: {person_profile}. Use each value and their respective keys from the provided object as the description for the context of the cover letter.",
+            max_tokens=1000,
+            temperature=0.75,
+        )
+        result = response.choices[0].text
 
-    return render_template("index.html")
+    return render_template("index.html", result=result)
