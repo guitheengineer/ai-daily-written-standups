83b8c1faa0ee49e640dfc4ca50e152c773936b2b
guilhermefront <guilhermesnd14@gmail.com>
2023-03-29 01:01:13 -0300

    feat: add initial boilerplate

--- app.py
+++ app.py
@@ -9,37 +9,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    body = request.args.to_dict()
-    person_profile = dict()
-    person_profile["workExperience"] = []
-    person_profile["education"] = []
+    article_content = ""
 
     result = ""
 
-    for key in body:
-        if "job" in key:
-            jobIndex = int(key.split("-")[0].replace("job", ""))
-            jobKey = key.split("-")[1]
-            try:
-                person_profile["workExperience"][jobIndex - 1][jobKey] = body[key]
-            except:
-                person_profile["workExperience"].insert(jobIndex, {jobKey: body[key]})
-
-        elif "education" in key:
-            jobIndex = int(key.split("-")[0].replace("education", ""))
-            jobKey = key.split("-")[1]
-            try:
-                person_profile["education"][jobIndex - 1][jobKey] = body[key]
-            except:
-                person_profile["education"].insert(jobIndex, {jobKey: body[key]})
-
-        else:
-            person_profile[key] = request.args.getlist(key)
-
-    if len(body.values()) > 0:
+    if article_content:
         response = openai.Completion.create(
             model="text-curie-001",
-            prompt=f"Generate a cover letter for me in first person. Here is some profile information about myself and the company as an object: {person_profile}. Use each value and their respective keys from the provided object as the description for the context of the cover letter.",
+            prompt=f"Write a keyword for the following article:",
             max_tokens=1000,
             temperature=0.75,
         )
